4d913912a9c19b2537d81560aafae111dd537e8c
archanakarne845-hub <archanakarne845@gmail.com>
2026-07-07 11:21:50 +0530

    Delete project.d

--- project.d
+++
@@ -1,7 +0,0 @@
-.\project.o: project.c
-.\project.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
-.\project.o: kpm_defines.h
-.\project.o: lcd_defines.h
-.\project.o: LCD.h
-.\project.o: rtc_main.h
-.\project.o: types.h
