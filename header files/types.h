373707267d9c45d812cd558373a5bce676765e34
archanakarne845-hub <archanakarne845@gmail.com>
2026-07-09 10:29:17 +0530

    Update types.h

--- header files/types.h
+++ header files/types.h
@@ -1,2 +1,7 @@
+//---------------------- Data Type Definitions ----------------------
+
+// Defines 'u32' as a 32-bit unsigned integer
 typedef unsigned int u32;
+
+// Defines 's32' as a 32-bit signed integer
 typedef signed int s32;
