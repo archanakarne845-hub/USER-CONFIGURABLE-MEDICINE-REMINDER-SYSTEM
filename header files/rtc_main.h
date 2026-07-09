a21c228108c60596e09f87eb423aa2f5cb084746
archanakarne845-hub <archanakarne845@gmail.com>
2026-07-09 10:27:32 +0530

    Update rtc_main.h

--- header files/rtc_main.h
+++ header files/rtc_main.h
@@ -20,15 +20,15 @@
 #define PREFRAC_VAL (PCLK -((PREINT_VAL + 1) * 32768))
 
 //RTC Control Register (CCR) Bit Definitions
-// Bit 0 – Clock Enable --> 1 = Enable RTC counters  0 = Disable RTC counters
+// Bit 0 â€“ Clock Enable --> 1 = Enable RTC counters  0 = Disable RTC counters
 #define RTC_ENABLE (1<<0)
 
-// Bit 1 – Clock Reset --> 1 = Reset RTC counters    0 = Normal operation
+// Bit 1 â€“ Clock Reset --> 1 = Reset RTC counters    0 = Normal operation
 #define RTC_RESET (1<<1)
  
 
 //only for LPC2148
-// Bit 4 – Clock Source Select 
+// Bit 4 â€“ Clock Source Select 
 // 1 = Use external 32.768 kHz oscillator
 // 0 = Use internal PCLK as RTC clock source
 #define RTC_CLKSRC (1<<4)
@@ -42,16 +42,31 @@
 #define FRI 5
 #define SAT 6
 
+// Initializes the Real-Time Clock (RTC)
 void RTC_Init(void);
+
+// Reads the current RTC time (Hour, Minute, Second)
 void GetRTCTimeInfo(s32 *,s32 *,s32 *);
+
+// Displays the current time on the LCD
 void DisplayRTCTime(u32,u32,u32);
+
+// Reads the current RTC date (Date, Month, Year)
 void GetRTCDateInfo(s32 *,s32 *,s32 *);
+
+// Displays the current date on the LCD
 void DisplayRTCDate(u32,u32,u32);
 
+// Sets the RTC time (Hour, Minute, Second)
 void SetRTCTimeInfo(u32,u32,u32);
-void SetRTCDateInfo(u32,u32,u32);
 
+// Reads the current day of the week
 void GetRTCDay(s32 *);
+
+// Displays the current day (Sunday to Saturday) on the LCD
 void DisplayRTCDay(u32);
+
+// Sets the current day of the week
 void SetRTCDay(u32);
+
 
